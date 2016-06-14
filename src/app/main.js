e0de4d921d42d9df385738436dd92231465de6fc
Cory Tohill <corytohill@gmail.com>
2016-06-14 11:30:12 -0500

    suggested pairings and user's selected option displayed on page

--- src/app/main.js
+++ src/app/main.js
@@ -1,26 +1,36 @@
 angular.module('app', ['ngRoute', 'ui.bootstrap'])
   .controller('PairingCtrl', function (FoodPairingFactory) {
     const pairing = this;
-    pairing.searchedIngredients = ''
+    pairing.searchedIngredients = [];
+    pairing.ingredientIds = [];
+    pairing.selectedIngredients = [];
 
+
+    // calls API search for available foods based on user text
     pairing.searchIngredients = function (ingredient) {
-      console.log(ingredient);
       FoodPairingFactory.ingredientInfo(ingredient)
         .then((data) => {
           return pairing.searchedIngredients = data;
-        }).then(console.log)
-    }
+        })
+    };
 
-    pairing.addIngredient = function (test) {
-      console.log(test);
-    }
+    pairing.addIngredient = function (ingredient) {
+      ingredient = JSON.parse(ingredient);
+      pairing.selectedIngredients.push(ingredient.name);
+
+      pairing.ingredientIds.push(ingredient.id);
+      FoodPairingFactory.suggestPairings(pairing.ingredientIds)
+        .then((data) => {
+          return pairing.pairings = data;
+        })
+    };
 
   })
 
   .factory('FoodPairingFactory', ($http) => {
     return {
       ingredientInfo (ingredient) {
-        let request = {
+        const searchRequest = {
           method: 'GET',
           url: `https://api.foodpairing.com/ingredients?q=${ingredient}?order=matches[all][rel]`,
           headers: {
@@ -29,7 +39,20 @@ angular.module('app', ['ngRoute', 'ui.bootstrap'])
           }
         };
 
-        return $http(request).then((response) => data = response.data)
+        return $http(searchRequest).then((response) => data = response.data)
+      },
+
+      suggestPairings (ingredientIds) {
+        const Ids = ingredientIds.join();
+        const pairingRequest = {
+          method: 'GET',
+          url: `https://api.foodpairing.com/ingredients/${Ids}/pairings?order=matches[all][rel]`,
+          headers: {
+            'X-Application-ID': '83dc83f5',
+            'X-Application-Key': 'f1a7be912dca2656623166a8a1715478'
+          }
+        }
+        return $http(pairingRequest).then((response) => data = response.data);
       }
     }
   })
