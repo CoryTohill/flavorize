c834701c68478529dd4da1ea8cea2886e6b55c4d
Cory Tohill <corytohill@gmail.com>
2016-07-05 20:46:28 -0500

    added delete button to user recipe cards

--- src/app/userHome/userHome.ctrl.js
+++ src/app/userHome/userHome.ctrl.js
@@ -1,6 +1,6 @@
 angular.module('app')
 
-  .controller('UserHomeCtrl', function (AuthFactory, UserRecipe, $location) {
+  .controller('UserHomeCtrl', function (AuthFactory, UserRecipe, $location, deleteRecipeFactory) {
     const userHome = this;
     const currentUser = firebase.auth().currentUser.uid;
 
@@ -8,11 +8,16 @@ angular.module('app')
       .then((response) => userHome.userRecipes = response);
 
     userHome.newRecipe = function () {
+      // resets the current recipe
       UserRecipe.setRecipe({"ingredients": []});
+      // resets the recipe key
+      UserRecipe.setRecipeKey("");
+
       $location.path('/recipeEditor');
     }
 
     userHome.editRecipe = function (recipe) {
+      // determines the unique firebase key for the recipe selected
       const recipesValues = Object.values(userHome.userRecipes);
       const recipesKeys = Object.keys(userHome.userRecipes);
       const index = recipesValues.indexOf(recipe);
@@ -23,6 +28,19 @@ angular.module('app')
       UserRecipe.setRecipe(recipe);
 
       $location.path('/recipeEditor')
+    }
+
+    userHome.deleteRecipe = function (recipe) {
+      // determines the unique firebase key for the recipe selected
+      const recipesValues = Object.values(userHome.userRecipes);
+      const recipesKeys = Object.keys(userHome.userRecipes);
+      const index = recipesValues.indexOf(recipe);
+      const key = recipesKeys[index];
+
+      deleteRecipeFactory.delete(key)
+        // updates the recipes on the current page after the selected recipe is deleted
+        .then(() => UserRecipe.getUserRecipes(currentUser))
+        .then((response) => userHome.userRecipes = response);
     }
 
     userHome.viewRecipe = function (recipe) {
