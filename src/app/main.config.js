79347d7d4d4163f0211630d1dc2189ed19855d80
Cory Tohill <corytohill@gmail.com>
2016-08-15 10:43:50 -0500

    more path types fixed

--- src/app/main.config.js
+++ src/app/main.config.js
@@ -5,26 +5,26 @@
       .when('/', {
         controller: 'HomePageCtrl',
         controllerAs: 'home',
-        templateUrl: '/app/homePage/homePage.html'
+        templateUrl: 'app/homePage/homePage.html'
       })
       .when('/recipeEditor', {
         controller: 'RecipeEditorCtrl',
         controllerAs: 'recipeEditor',
-        templateUrl: '/app/recipeEditor/recipeEditor.html'
+        templateUrl: 'app/recipeEditor/recipeEditor.html'
       })
       .when('/login', {
         controller: 'LoginCtrl',
         controllerAs: 'auth',
-        templateUrl: '/app/login/login.html'
+        templateUrl: 'app/login/login.html'
       })
       .when('/userHome', {
         controller: 'UserHomeCtrl',
         controllerAs: 'userHome',
-        templateUrl: '/app/userHome/userHome.html'
+        templateUrl: 'app/userHome/userHome.html'
       })
       .when('/viewRecipe', {
         controller: 'ViewRecipeCtrl',
         controllerAs: 'view',
-        templateUrl: '/app/viewRecipe/viewRecipe.html'
+        templateUrl: 'app/viewRecipe/viewRecipe.html'
       })
   ));
